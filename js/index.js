68dd3687c4069398bf631e87886fad57f47d7d8a
Pedro Barri <barripedro88@gmail.com>
2020-05-10 15:57:40 -0300

    Dynamic grid size. Retro styling. Refactor.

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
-const getTetrominos = (gridWidth) => {
+const getTetrominos = (blocksPerRow) => {
   //The Tetrominoes
   const lTetromino = [
-    [1, gridWidth + 1, gridWidth * 2 + 1, 2],
-    [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 2],
-    [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 2],
-    [gridWidth, gridWidth * 2, gridWidth * 2 + 1, gridWidth * 2 + 2],
+    [1, blocksPerRow + 1, blocksPerRow * 2 + 1, 2],
+    [blocksPerRow, blocksPerRow + 1, blocksPerRow + 2, blocksPerRow * 2 + 2],
+    [1, blocksPerRow + 1, blocksPerRow * 2 + 1, blocksPerRow * 2],
+    [blocksPerRow, blocksPerRow * 2, blocksPerRow * 2 + 1, blocksPerRow * 2 + 2],
   ];
 
   const zTetromino = [
-    [0, gridWidth, gridWidth + 1, gridWidth * 2 + 1],
-    [gridWidth + 1, gridWidth + 2, gridWidth * 2, gridWidth * 2 + 1],
-    [0, gridWidth, gridWidth + 1, gridWidth * 2 + 1],
-    [gridWidth + 1, gridWidth + 2, gridWidth * 2, gridWidth * 2 + 1],
+    [0, blocksPerRow, blocksPerRow + 1, blocksPerRow * 2 + 1],
+    [blocksPerRow + 1, blocksPerRow + 2, blocksPerRow * 2, blocksPerRow * 2 + 1],
+    [0, blocksPerRow, blocksPerRow + 1, blocksPerRow * 2 + 1],
+    [blocksPerRow + 1, blocksPerRow + 2, blocksPerRow * 2, blocksPerRow * 2 + 1],
   ];
 
   const tTetromino = [
-    [1, gridWidth, gridWidth + 1, gridWidth + 2],
-    [1, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 1],
-    [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth * 2 + 1],
-    [1, gridWidth, gridWidth + 1, gridWidth * 2 + 1],
+    [1, blocksPerRow, blocksPerRow + 1, blocksPerRow + 2],
+    [1, blocksPerRow + 1, blocksPerRow + 2, blocksPerRow * 2 + 1],
+    [blocksPerRow, blocksPerRow + 1, blocksPerRow + 2, blocksPerRow * 2 + 1],
+    [1, blocksPerRow, blocksPerRow + 1, blocksPerRow * 2 + 1],
   ];
 
   const oTetromino = [
-    [0, 1, gridWidth, gridWidth + 1],
-    [0, 1, gridWidth, gridWidth + 1],
-    [0, 1, gridWidth, gridWidth + 1],
-    [0, 1, gridWidth, gridWidth + 1],
+    [0, 1, blocksPerRow, blocksPerRow + 1],
+    [0, 1, blocksPerRow, blocksPerRow + 1],
+    [0, 1, blocksPerRow, blocksPerRow + 1],
+    [0, 1, blocksPerRow, blocksPerRow + 1],
   ];
 
   const iTetromino = [
-    [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 3 + 1],
-    [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth + 3],
-    [1, gridWidth + 1, gridWidth * 2 + 1, gridWidth * 3 + 1],
-    [gridWidth, gridWidth + 1, gridWidth + 2, gridWidth + 3],
+    [1, blocksPerRow + 1, blocksPerRow * 2 + 1, blocksPerRow * 3 + 1],
+    [blocksPerRow, blocksPerRow + 1, blocksPerRow + 2, blocksPerRow + 3],
+    [1, blocksPerRow + 1, blocksPerRow * 2 + 1, blocksPerRow * 3 + 1],
+    [blocksPerRow, blocksPerRow + 1, blocksPerRow + 2, blocksPerRow + 3],
   ];
 
   return [
@@ -48,28 +48,80 @@ const getRandom = length => {
   return Math.floor(Math.random() * length);
 };
 
-document.addEventListener('DOMContentLoaded', () =>{
-  const gridWidth = 10;
-  const gridHeight = 20;
-  const gridSize = gridWidth * gridHeight;
-  let currentRotation = 0;
-  const grid = document.querySelector('.grid');
-  grid.setAttribute(
-    'style',
-    `height: ${gridHeight * gridHeight}px; width: ${gridWidth * gridHeight}px;`
-  );
+const createGrid = (grid, blocksPerRow) => {
+  const {
+    squareBlockSize,
+    gridHeight,
+    gridWidth,
+    blocksPerColumn,
+  } = getDimensiones();
+  const squaresAmount = blocksPerRow * blocksPerColumn;
+  for (let i = 0; i < squaresAmount; i += 1) {
+    let div = document.createElement('div');
+    div.setAttribute(
+      'style',
+      `height: ${squareBlockSize}px; width: ${squareBlockSize}px;background-repeat: round;`
+    );
+    if (i >= squaresAmount - blocksPerRow) {
+      div.setAttribute('class', 'green-block');
+    }
+    grid.appendChild(div);
+  }
   const previousGrid = document.querySelector('.previous-grid');
   const previousShape = document.querySelector('.previous-shape');
+  for (let i = 0; i < 16; i += 1) {
+    let div = document.createElement('div');
+    div.setAttribute(
+      'style',
+      `height: ${squareBlockSize}px; width: ${squareBlockSize}px;background-repeat: round;`
+    );
+    previousGrid.appendChild(div);
+  }
+  grid.setAttribute(
+    'style',
+    `height: ${gridHeight}px; width: ${gridWidth}px;`
+  );
   previousGrid.setAttribute(
     'style',
-    `height: ${gridHeight*4}px; width: ${gridHeight*4}px;`
+    `height: ${squareBlockSize * 4}px; width: ${squareBlockSize * 4}px;`
   );
   previousShape.setAttribute(
     'style',
-    `height: ${gridHeight*4}px; width: ${gridHeight*4}px;`
+    `height: ${squareBlockSize * 4}px; width: ${squareBlockSize * 4}px;`
   );
+};
+
+const getDimensiones = () => {
+  const squareBlockSize = 20;
+  const height = window.innerHeight;
+  const gridHeight = Math.round(height / 100) * 100 - 200;
+  const gridWidth = gridHeight / 2;
+  const blocksPerColumn = gridHeight / squareBlockSize;
+  return {
+    squareBlockSize,
+    gridHeight,
+    gridWidth,
+    blocksPerColumn,
+  };
+};
+
+const setGrid = (grid) => {
+  const { squareBlockSize, gridWidth, blocksPerColumn } = getDimensiones();
+  const blocksPerRow = gridWidth / squareBlockSize;
+  createGrid(grid, blocksPerRow);
+  return { blocksPerRow, blocksPerColumn };
+};
+
+document.addEventListener('DOMContentLoaded', () =>{
+  let currentRotation = 0;
+  const grid = document.querySelector('.grid');
+  const { blocksPerRow, blocksPerColumn } = setGrid(grid);
+  const initTimeIntervalLevel = 1000;
+  let currentLevel = 0;
+    
   const scoreDisplay = document.querySelector('.score-display');
   const linesDisplay = document.querySelector('.lines-display');
+  const levelDisplay = document.querySelector('.level-display');
   const startBtn = document.querySelector('button');
   let timerId;
   let score = 0;
@@ -87,33 +139,11 @@ document.addEventListener('DOMContentLoaded', () =>{
       moveDown();
     } 
   }
-  document.addEventListener('keyup', control);
+  document.addEventListener('keydown', control);
 
-  const createGrid = (gridSize) => {
-    for (let i = 0; i < gridSize; i += 1) {
-      let div = document.createElement('div');
-      div.setAttribute(
-        'style',
-        `height: ${gridHeight}px; width: ${gridHeight}px;background-repeat: round;`
-      );
-      if (i >= (gridSize - gridWidth)) {
-        div.setAttribute('class', 'green-block');
-      }
-      grid.appendChild(div);
-    }
-    for (let i = 0; i < 16; i += 1) {
-      let div = document.createElement('div');
-      div.setAttribute(
-        'style',
-        `height: ${gridHeight}px; width: ${gridHeight}px;background-repeat: round;`
-      );
-      previousGrid.appendChild(div);
-    }
-  };
-  createGrid(gridSize);
   let squares = Array.from(grid.querySelectorAll('div'));
 
-  const tetrominos = getTetrominos(gridWidth);
+  const tetrominos = getTetrominos(blocksPerRow);
 
   // Random
   let random = getRandom(tetrominos.length);
@@ -134,15 +164,14 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   const moveDown = () => {
     undraw();
-    currentPosition = currentPosition + gridWidth;
+    currentPosition = currentPosition + blocksPerRow;
     draw();
     freeze();
   };
 
   const moveRight = () => {
     undraw();
-    const isAtRightEdge = current.some(index => (currentPosition + index) % gridWidth === gridWidth - 1);
-    if (!isAtRightEdge) currentPosition += 1;
+    if (!isAtRightEdge()) currentPosition += 1;
     if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
       currentPosition -= 1;
     }
@@ -151,8 +180,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   const moveLeft = () => {
     undraw();
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % gridWidth === 0);
-    if (!isAtLeftEdge) currentPosition -= 1;
+    if (!isAtLeftEdge()) currentPosition -= 1;
     if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
       currentPosition += 1;
     }
@@ -166,8 +194,14 @@ document.addEventListener('DOMContentLoaded', () =>{
       currentRotation = 0;
     }
     current = tetrominos[random][currentRotation];
+    while (isAtRightEdge() && isAtLeftEdge()) {
+      currentPosition%10 === 9 ? currentPosition+=1 : currentPosition-= 1;
+    }
     draw();
   };
+
+  const isAtRightEdge = () => current.some(index => (currentPosition + index) % blocksPerRow === blocksPerRow - 1);
+  const isAtLeftEdge = () => current.some(index => (currentPosition + index) % blocksPerRow === 0);
   
   // Display
   const displayWidth = 4;
@@ -186,10 +220,16 @@ document.addEventListener('DOMContentLoaded', () =>{
     });
   };
 
+  const clearShape = () => {
+    smallTetrominos[nextRandom].forEach(index => {
+      displaySquares[displayIndex + index].classList.remove(`${colors[nextRandom]}-block`)
+    });
+  };
+
   const freeze = () => {
     if (current.some(index => 
-      squares[currentPosition + index + gridWidth].classList.contains('green-block') || 
-      squares[currentPosition + index + gridWidth].classList.contains('block2'))
+      squares[currentPosition + index + blocksPerRow].classList.contains('green-block') || 
+      squares[currentPosition + index + blocksPerRow].classList.contains('block2'))
     ) {
       current.forEach(index => squares[index + currentPosition].classList.add('block2'))
       random = nextRandom;
@@ -203,19 +243,47 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
   };
 
+  const displayStartButton = () => {
+    startBtn.classList.add('btn-danger');
+    startBtn.classList.remove('btn-warning');
+    startBtn.innerHTML = 'Start';
+  };
+
+  const displayPauseButton = () => {
+    startBtn.classList.remove('btn-danger');
+    startBtn.classList.add('btn-warning');
+    startBtn.innerHTML = 'Pause';
+  };
+
   startBtn.addEventListener('click', () => {
     if (timerId) {
       clearInterval(timerId);
       timerId = null;
+      clearShape();
+      displayStartButton();
     } else {
+      currentLevel = 0;
       draw();
       timerId = setInterval(() => {
         moveDown();
-      }, 1000);
+      }, initTimeIntervalLevel);
       nextRandom = getRandom(tetrominos.length);
       displayShape();
+      displayPauseButton();
     }
   });
+
+  const increaseLevel = (level) => {
+    if (timerId) {
+      clearInterval(timerId);
+    }
+    const timeIntervalLevel = initTimeIntervalLevel / (level + 1);
+    timerId = setInterval(() => {
+      moveDown();
+    }, timeIntervalLevel);
+    console.log('level UP!', level)
+    levelDisplay.innerHTML = level;
+  }
 
   const gameOver = () => {
     if (current.some(index => squares[currentPosition + index].classList.contains('block2'))) {
@@ -225,8 +293,9 @@ document.addEventListener('DOMContentLoaded', () =>{
   };
 
   const addScore = () => {
-    for (currentIndex = 0; currentIndex < 199; currentIndex += gridWidth) {
-      const row = [currentIndex, currentIndex + 1, currentIndex + 2, currentIndex + 3, currentIndex + 4, currentIndex + 5, currentIndex + 6, currentIndex + 7, currentIndex + 8, currentIndex + 9];
+    const keys = Array.from(Array(blocksPerRow).keys());
+    for (currentIndex = 0; currentIndex < (blocksPerRow * blocksPerColumn) - 1; currentIndex += blocksPerRow) {
+      const row = Array.from(keys.map(key => key + currentIndex));
       
       if (row.every(index => squares[index].classList.contains('block2'))) {
         score +=10;
@@ -235,12 +304,16 @@ document.addEventListener('DOMContentLoaded', () =>{
         linesDisplay.innerHTML = lines;
 
         row.forEach(index => {
-          squares[index].classList.remove('block2') || squares[index].classList.remove(`${colors[random]}-block`)
+          squares[index].classList.remove(...squares[index].classList)
         });
         // splice
-        const squaresRemoved = squares.splice(currentIndex, gridWidth);
+        const squaresRemoved = squares.splice(currentIndex, blocksPerRow);
         squares = squaresRemoved.concat(squares);
         squares.forEach(cell => grid.appendChild(cell));
+        if (score % 50 === 0) {
+          currentLevel = score / 50;
+          increaseLevel(currentLevel);
+        }
       }
     }
   };
